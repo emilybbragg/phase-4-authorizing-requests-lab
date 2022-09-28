286aaebea230face43893fd3e8fc1f923aae67d5
Emily <emilybribragg@gmail.com>
2022-09-28 09:30:26 -0400

    commit tree 869892f8c69c6ad54b5c6e0215dcdf2c059e2177

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -9,6 +10,10 @@ class MembersOnlyArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: article
+  end
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 
   private
